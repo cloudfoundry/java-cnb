71f02b91d43670bc6c2859c390d628de268ac49e
Ben Hale <bhale@pivotal.io>
2018-09-21 14:56:23 -0700

    Build
    This change fills out the build script to untar the buildpack artifacts and
    call the pack CLI to created the builder.

--- ci/build.sh
+++ ci/build.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
-find .
+tar xzvf build-system-buildpack-artifactory/*.tgz -C build-system-buildpack
+tar xzvf jvm-application-buildpack-artifactory/*.tgz -C jvm-application-buildpack
+tar xzvf openjdk-buildpack-artifactory/*.tgz -C openjdk-buildpack
+
+pack/pack-linux create-builder cfje/java-buildpack-group --builder-config java-buildpack-group/builder.toml
