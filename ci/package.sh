38e629a207e31a134cc402cdd7ef2146b75ad87e
Ben Hale <bhale@pivotal.io>
2018-10-03 15:52:24 -0700

    Package Buildpack Group

--- ci/package.sh
+++ ci/package.sh
@@ -9,12 +9,10 @@ extract() {
   tar xzf $1-artifactory/**/*.tgz -C $1
 }
 
-source $(dirname $0)/docker.sh
-start_docker "" "" ""
-
 extract build-system-buildpack
 extract jvm-application-buildpack
 extract openjdk-buildpack
 
-chmod +x pack/pack-linux
-pack/pack-linux create-builder cfje/java-buildpack-group --builder-config java-buildpack-group/builder.toml
+tar czvf "$PWD/artifactory/java-buildpack-group.tgz" -C . build-system-buildpack jvm-application-buildpack openjdk-buildpack -C java-buildpack-group builder.toml
+
+tar tvf "$PWD/artifactory/java-buildpack-group.tgz"
